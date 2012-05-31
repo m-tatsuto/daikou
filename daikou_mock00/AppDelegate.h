ca9fd49c45f7bdb867fc6ae78ac1c6bc19ec478c
Tatsuto MAETSU <m.tatsuto@gmail.com>
2012-05-31 13:22:23 +0900

    Initial Commit

---
+++ daikou_mock00/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  daikou_mock00
+//
+//  Created by 達人 前津 on 12/05/31.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ daikou_mock00/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  daikou_mock00
+//
+//  Created by 達人 前津 on 12/05/31.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
